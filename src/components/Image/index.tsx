707d73bdbf0eb6191647c07007a03fd6ac7acc71
ahmed <ahmedmijdo2@gmail.com>
2025-03-25 01:09:24 +0200

    finshed landing home page

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,13 +1,33 @@
-import landingImage from './../../assets/landing image.jpg';
+import './index.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagic } from "@fortawesome/free-solid-svg-icons";
+import { faGem } from "@fortawesome/free-solid-svg-icons";
+import { faGlobeAsia } from "@fortawesome/free-solid-svg-icons";
+
 
 function Image () {
     return (
         <>
-            <div>
-                <img style={{
-                    width: '100%',
-                    backgroundSize: 'contain',
-                }} src={landingImage} alt="landingImage" />
+            <div className='landing-image'>
+                <h1>Hello there</h1>
+                <h3>We are Leon - Super Creative & Minimal Agancy Web Template</h3>
+            </div>
+            <div className='icons'>
+                <div className='item-1 card'>
+                    <FontAwesomeIcon className='font-awesome' icon={faMagic}/>
+                    <h3>Tell Us your Idea</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, minus!</p>
+                </div>
+                <div className='item-2 card'>
+                    <FontAwesomeIcon className='font-awesome' icon={faGem}/>
+                    <h3>Tell Us your Idea</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, minus!</p>
+                </div>
+                <div className='item-3 card'>
+                    <FontAwesomeIcon className='font-awesome' icon={faGlobeAsia}/>
+                    <h3>Tell Us your Idea</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, minus!</p>
+                </div>
             </div>
         </>
     );
